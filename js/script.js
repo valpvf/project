fdf822b526804931d11c5a23eb99b7141ea340af
Valerii <p_vf@i.ua>
2020-07-06 14:43:46 +0300

    remove adv, change genre, add films from DB

--- js/script.js
+++ js/script.js
@@ -24,3 +24,28 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    // adv.forEach(function (item) {   // в обычном (не стрелочном) варианте
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url(img/bg.jpg)';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
