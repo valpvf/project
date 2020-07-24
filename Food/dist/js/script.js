48832eb38de1d59c8529864029b9009b1286ffd5
Valerii <p_vf@i.ua>
2020-07-24 18:17:23 +0300

    add send data from forms (XMLHttpRequest in formData)

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -216,4 +216,49 @@ window.addEventListener('DOMContentLoaded', () => {
         'menu__item'
     ).render();
 
+    // Forms POST
+
+    const forms = document.querySelectorAll('form'),
+        message = {
+            loading: 'Загрузка',
+            success: 'Спасибо! Скоро мы с Вами свяжемся ',
+            failure: 'Что-то пошло не так ...'
+        };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+
+            const formData = new FormData(form);
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                        closeModal();
+                    }, 2000);                    
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
+
 });
